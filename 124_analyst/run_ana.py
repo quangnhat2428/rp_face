80da2acf1ef1fe248601f2160270130dc989c787
NhatQuangNguyen-git <NhatQuangNguyen-git@users.noreply.github.com>
2024-08-21 20:13:10 +0700

    Update run_ana.py

--- 124_analyst/run_ana.py
+++ 124_analyst/run_ana.py
@@ -1,7 +1,7 @@
 import shutil
 
 # Đường dẫn nguồn
-source_path = '/content/rp_face/124_analyst/face_analyser.py'
+source_path = '/content/rp_face_/124_analyst/face_analyser.py'
 
 # Đường dẫn đích
 destination_path = '/content/extracted/124/facefusion/face_analyser.py'
